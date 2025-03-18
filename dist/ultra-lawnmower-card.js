bb7e444d4deb77ffc6f041bc309d2f3871595597
lukash97 <79584197+lukash97@users.noreply.github.com>
2025-03-18 21:15:47 +0100

    Version angepasst

--- dist/ultra-lawnmower-card.js
+++ dist/ultra-lawnmower-card.js
@@ -4,7 +4,7 @@ import {
   css,
 } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 import { until } from "https://unpkg.com/lit-html@1.4.1/directives/until.js?module";
-import { version, setVersion } from "./version.js?v=39";
+import { version, setVersion } from "./version.js?v=1";
 setVersion("V1.0.1");
 
 const sensorModule = await import("./sensors.js?v=" + version);

f56627acc2534be9052f8d9fbe847be35f256a46
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-03-18 22:12:10 +0100

    remove languages

--- dist/ultra-lawnmower-card.js
+++ dist/ultra-lawnmower-card.js
@@ -4,8 +4,8 @@ import {
   css,
 } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 import { until } from "https://unpkg.com/lit-html@1.4.1/directives/until.js?module";
-import { version, setVersion } from "./version.js?v=4";
-setVersion("V1.0.4");
+import { version, setVersion } from "./version.js?v=5";
+setVersion("V1.0.5");
 
 const sensorModule = await import("./sensors.js?v=" + version);
 const {
